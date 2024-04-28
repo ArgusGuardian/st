bef4b44d4aeca3bf572205f497a3814fea782931
Alaa Eddine <Alaaelhannaoui@proton.me>
2024-04-28 23:56:51 +0100

    cattpuccin clorscheme

--- config.h
+++ config.h
@@ -113,17 +113,40 @@ float alpha = 0.8;
 float alphaOffset = 0.0;
 float alphaUnfocus;
 
+// /* Terminal colors (16 first used in escape sequence) */
+// static const char *colorname[] = {
+//     "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+//     "#cc241d", "#98971a", "#d79921", "#458588", "#b16286", "#689d6a", "#a89984",
+//     "#928374", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c",
+//     "#ebdbb2", [255] = 0,
+//     /* more colors can be added after 255 to use with DefaultXX */
+//     "#add8e6", /* 256 -> cursor */
+//     "#555555", /* 257 -> rev cursor*/
+//     "#282828", /* 258 -> bg */
+//     "#ebdbb2", /* 259 -> fg */
+// };
+//
+// /*
+//  * Default colors (colorname index)
+//  * foreground, background, cursor, reverse cursor
+//  */
+// unsigned int defaultfg = 259;
+// unsigned int defaultbg = 258;
+// unsigned int defaultcs = 256;
+// unsigned int defaultrcs = 257;
+// unsigned int background = 258;
+//
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-    "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-    "#cc241d", "#98971a", "#d79921", "#458588", "#b16286", "#689d6a", "#a89984",
-    "#928374", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c",
-    "#ebdbb2", [255] = 0,
+    "#45475A", "#F38BA8", "#A6E3A1", "#F9E2AF", "#89B4FA", "#F5C2E7", "#94E2D5", "#BAC2DE",
+    "#585B70", "#F38BA8", "#A6E3A1", "#F9E2AF", "#89B4FA", "#F5C2E7", "#94E2D5", "#A6ADC8",
+    [255] = 0,
     /* more colors can be added after 255 to use with DefaultXX */
-    "#add8e6", /* 256 -> cursor */
+    "#F5E0DC", /* 256 -> cursor */
     "#555555", /* 257 -> rev cursor*/
-    "#282828", /* 258 -> bg */
-    "#ebdbb2", /* 259 -> fg */
+    "#1e1e2e", /* 258 -> bg */
+    "#CDD6F4", /* 259 -> fg */
 };
 
 /*
@@ -135,7 +158,6 @@ unsigned int defaultbg = 258;
 unsigned int defaultcs = 256;
 unsigned int defaultrcs = 257;
 unsigned int background = 258;
-
 /*
  * Default shape of cursor
  * 2: Block ("█")
